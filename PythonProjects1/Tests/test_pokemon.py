7ce8e98f8a551731e0522834b9effcdd1f3b0c7b
PirozhokQA <159764465+PirozhokQA@users.noreply.github.com>
2024-03-23 20:15:41 +0400

    Add files via upload

---
+++ PythonProjects1/Tests/test_pokemon.py
@@ -0,0 +1,17 @@
+import requests
+import pytest
+
+
+URL = 'https://api.pokemonbattle.me/'
+
+TOKEN = 'Токен'
+HEADERS = {'Content-Type' : 'application/json', 'trainer_token' : TOKEN}
+
+
+def test_status_code():
+    response = requests.get(url = f'{URL}/trainers', params = {"trainer_id" : 2114})
+    assert response.status_code == 200
+
+def test_part_of_response():
+    response = requests.get(url = f'{URL}/trainers', params = {"trainer_id" : 2114})
+    assert response.json()['trainer_name'] =='Cat'
